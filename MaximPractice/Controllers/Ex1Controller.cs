ca061f612cfc25cf018f82ef490005f1594e19a4
MKstii <101364255+MKstii@users.noreply.github.com>
2024-06-21 20:32:25 +0500

    Выполнил 1 задание

--- MaximPractice/Controllers/Ex1Controller.cs
+++ MaximPractice/Controllers/Ex1Controller.cs
@@ -13,5 +13,48 @@ namespace MaximPractice.Controllers
         {
             _logger = logger;
         }
+
+        [HttpGet]
+        public IActionResult Ex1(string str)
+        {
+            string res = str.Length % 2 == 0 ?
+                ConvertEvenString(str) : ConvertOddString(str);
+            return Ok(res);
+        }
+
+        private string ConvertEvenString(string str)
+        {
+            var strBuilder = new StringBuilder();
+
+            var firstHalf = ReverseString(str, 0, (str.Length - 1) / 2);
+            var secondHalf = ReverseString(str, str.Length / 2, str.Length-1);
+
+            strBuilder.Append(firstHalf);
+            strBuilder.Append(secondHalf);
+
+            return strBuilder.ToString();
+        }
+
+        private string ConvertOddString(string str)
+        {
+            var strBuilder = new StringBuilder();
+
+            var firstHalf = ReverseString(str, 0, str.Length-1);
+
+            strBuilder.Append(firstHalf);
+            strBuilder.Append(str);
+
+            return strBuilder.ToString();
+        }
+
+        private string ReverseString(string str, int start, int end)
+        {
+            var strBuilder = new StringBuilder();
+            for (int i = end; i >= start; i--)
+            {
+                strBuilder.Append(str[i]);
+            }
+            return strBuilder.ToString();
+        }
     }
 }
